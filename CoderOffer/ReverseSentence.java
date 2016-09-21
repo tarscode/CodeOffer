082e31f5bb17717a720df0cbbbbf5b5178fa8243
tarsdroid <tarsdroid@hotmail.com>
2016-09-21 16:47:59 +0800

    翻转单词顺序列 V1.1 用翻转函数代替栈

--- CoderOffer/ReverseSentence.java
+++ CoderOffer/ReverseSentence.java
@@ -1,45 +1,40 @@
-import java.util.Stack;
-
 /**
  * 工程: CodeOffer 包名: PACKAGE_NAME 类名: ReverseSentence
  * 作者: liuyang
  * 时间: 16/9/21 下午3:03
  * 题目: 翻转单词顺序列
  * 内容:
- * 版本: V1.0
+ * 版本: V1.1
  * 运行时间:
- * 备注:
+ * 备注: 用翻转函数代替栈，额外空间复杂度O(1)
  */
 public class ReverseSentence {
     public String ReverseSentence(String str) {
         char[] arr = str.toCharArray();
-        int len = arr.length;
-        int index = 0;
-        Stack wordStack = new Stack();
-        Stack strStack = new Stack();
-        for (int i = 0; i < len; i++) {
-            //以空格为分界用栈两次处理每一个单词
-            if (arr[i] == ' ') {
-                while (!wordStack.empty() && arr[i] == ' ') {
-                    strStack.push(wordStack.pop());
-                }
-                strStack.push(arr[i]);
-            } else {
-                wordStack.push(arr[i]);
-
+        int len = str.length();
+        int begin =0;
+        int end;
+        for(int i=0;i<len;i++){
+            if(arr[i]==' '){
+                end = i;
+                Reverse(arr,begin,end-1);
+                begin=end+1;
             }
-            //处理最后一个单词
-            if (i == len - 1) {
-                while (!wordStack.empty()) {
-                    strStack.push(wordStack.pop());
-                }
+            if(i==len-1){
+                Reverse(arr,begin,i);
             }
         }
-        while (!strStack.empty()) {
-            arr[index++] = (char) strStack.pop();
-        }
+        arr = Reverse(arr,0,len-1);
         str = String.valueOf(arr);
         return str;
+    }
+    public char[] Reverse(char[] arr,int begin,int end){
+        while(begin<end){
+            char temp = arr[begin];
+            arr[begin++]=arr[end];
+            arr[end--] = temp;
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
