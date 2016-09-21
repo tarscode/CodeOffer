df7b5e06b5e86277eb25fab9adc25a6dadfdd6c1
tarsdroid <tarsdroid@hotmail.com>
2016-09-21 10:21:30 +0800

    和为S的两个数字 V2.0 建立头尾指针，一趟搜索

--- CoderOffer/FindNumbersWithSum.java
+++ CoderOffer/FindNumbersWithSum.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 工程: CodeOffer 包名: PACKAGE_NAME 类名: FindNumbersWithSum
@@ -8,22 +6,23 @@ import java.util.Map;
  * 时间: 16/9/20 下午8:38
  * 题目: 和为S的两个数字
  * 内容:
- * 版本: V1.0
- * 运行时间: 32ms
- * 备注: 建立Map，两趟搜索
+ * 版本: V2.0
+ * 运行时间: 39ms
+ * 备注: 建立头尾指针，一趟搜索
  */
 public class FindNumbersWithSum {
     public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
-        Map map = new HashMap();
         ArrayList list = new ArrayList();
-        for(int i=0;i<array.length;i++){
-            map.put(array[i],i);
-        }
-        for(int i=0;i<array.length;i++){
-            int target = sum - array[i];
-            if(map.containsKey(target)){
-                list.add(array[i]);
-                list.add(target);
+        int left = 0;
+        int right = array.length-1;
+        while(left<right){
+            if(array[left]+array[right]>sum){
+                right--;
+            }else if(array[left]+array[right]<sum){
+                left++;
+            }else{
+                list.add(array[left]);
+                list.add(array[right]);
                 break;
             }
         }
