d9c99d8e329a90769dbd27ff841ed5cb8c207ff3
tarsdroid <tarsdroid@hotmail.com>
2016-09-26 22:22:26 +0800

    丑数

--- CoderOffer/GetUglyNumber.java
+++ CoderOffer/GetUglyNumber.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 /**
  * 【工程】: CodeOffer 包名: PACKAGE_NAME 类名: GetUglyNumber
@@ -6,38 +5,44 @@ import java.util.*;
  * 【时间】: 16/9/25 下午7:11
  * 【题目】: 丑数
  * 【内容】: 把只包含因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
- * 【版本】: V1.0
+ * 【版本】: V1.1
  * 【运行时间】:
- * 【时间复杂度】:
- * 【空间复杂度】:
+ * 【时间复杂度】:O(n)
+ * 【空间复杂度】:O(n)
  * 【备注】:
  * 【思路】:
  */
 public class GetUglyNumber {
     public int GetUglyNumber_Solution(int index) {
-        Set set = new HashSet();
-        set.add(1);
-        List<Integer> list = new ArrayList<>();
-        int[] arr = {2, 3, 5};
-        list.add(1);
-        int flag = 0;
-        while (list.size() <= index) {
-            for (int i = 0; i < arr.length && list.size() <= index; i++) {
-                int value = list.get(flag) * arr[i];
-                if (!set.contains(value)) {
-                    set.add(value);
-                    list.add(value);
-                }
-            }
-            flag++;
+        if(index<=0){
+            return 0;
         }
-        list.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1-o2;
+        int arr[] = new int[index];
+        arr[0] = 1;
+        int i = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        while (i < index) {
+            arr[i] = getMin(arr[index2] * 2, arr[index3] * 3, arr[index5] * 5);
+            while (arr[index2] * 2 <= arr[i]) {
+                ++index2;
             }
-        });
-        return list.get(index-1);
+            while (arr[index3] * 3 <= arr[i]) {
+                ++index3;
+            }
+            while (arr[index5] * 5 <= arr[i]) {
+                ++index5;
+            }
+            i++;
+        }
+        return arr[index - 1];
+    }
+
+    public int getMin(int num1, int num2, int num3) {
+        int min = num1 < num2 ? num1 : num2;
+        min = min < num3 ? min : num3;
+        return min;
     }
 
     public static void main(String[] args) {
