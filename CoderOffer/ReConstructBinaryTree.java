5cd2e40ab464b9e11031c4ad5c972ccc13ea179e
tarsdroid <tarsdroid@hotmail.com>
2016-10-02 20:23:09 +0800

    重建二叉树

--- CoderOffer/ReConstructBinaryTree.java
+++ CoderOffer/ReConstructBinaryTree.java
@@ -1,16 +1,35 @@
+import java.util.HashMap;
+
 /**
  * 工程: CodeOffer 包名: PACKAGE_NAME 类名: ReConstructBinaryTree
  * 作者: liuyang
  * 时间: 16/9/16 下午1:10
  * 题目: 重建二叉树
- * 内容:
+ * 内容: 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  * 版本:
  * 运行时间:
  * 备注:
  */
 public class ReConstructBinaryTree {
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        TreeNode head = new TreeNode(0);
+        if (pre == null || in == null) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < in.length; i++) {
+            map.put(in[i], i);
+        }
+        return preIn(pre, 0, pre.length - 1, in, 0, in.length - 1, map);
+    }
+
+    public TreeNode preIn(int[] p, int pi, int pj, int[] n, int ni, int nj, HashMap<Integer, Integer> map) {
+        if (pi > pj) {
+            return null;
+        }
+        TreeNode head = new TreeNode(p[pi]);
+        int index = map.get(p[pi]);
+        head.left = preIn(p, pi + 1, pi + index - ni, n, ni, index - 1, map);
+        head.right = preIn(p, pi + index - ni + 1, pj, n, index + 1, nj, map);
         return head;
     }
 
